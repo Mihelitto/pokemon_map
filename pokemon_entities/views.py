de0cba71e85b21b97cd09ac5aeb80db574fd3242
Michael Yudin <mihelitto@gmail.com>
2021-04-08 00:26:57 +0300

    Use a model to show pokemons

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
+from pokemon_entities.models import Pokemon
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = "https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832&fill=transparent"
@@ -25,6 +26,8 @@ def show_all_pokemons(request):
     with open("pokemon_entities/pokemons.json", encoding="utf-8") as database:
         pokemons = json.load(database)['pokemons']
 
+    pokemons_ = Pokemon.objects.all()
+
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon in pokemons:
         for pokemon_entity in pokemon['entities']:
@@ -32,11 +35,11 @@ def show_all_pokemons(request):
                 folium_map, pokemon_entity['lat'], pokemon_entity['lon'], pokemon['img_url'])
 
     pokemons_on_page = []
-    for pokemon in pokemons:
+    for pokemon in pokemons_:
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': request.build_absolute_uri(pokemon.image.url),
+            'title_ru': pokemon.title,
         })
 
     return render(request, "mainpage.html", context={
