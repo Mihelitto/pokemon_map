512c57a99a2e0d7346ac384c9ee3e8d4ec2545b9
Michael Yudin <mihelitto@gmail.com>
2021-04-13 00:46:03 +0300

    Set required fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 class Pokemon(models.Model):
     title = models.CharField("Название", max_length=200)
     title_en = models.CharField("Название на английском", max_length=200, default="")
     title_jp = models.CharField("Название на японском", max_length=200, default="")
-    image = models.ImageField("Изображение", upload_to="pokemons", blank=True, null=True)
-    description = models.TextField("Описание", max_length=500, blank=True, null=True)
+    image = models.ImageField("Изображение", upload_to="pokemons")
+    description = models.TextField("Описание", max_length=500, default="")
     previous_evolution = models.ForeignKey(
         "self",
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
         related_name="next_evolutions",
@@ -29,17 +30,17 @@ class PokemonEntity(models.Model):
     lat = models.FloatField("Широта")
     lon = models.FloatField("Долгота")
 
-    appeared_at = models.DateTimeField("Появился")
-    disappeared_at = models.DateTimeField("Исчезнет")
+    appeared_at = models.DateTimeField("Появился", blank=True, null=True)
+    disappeared_at = models.DateTimeField("Исчезнет", blank=True, null=True)
 
-    level = models.IntegerField("Уровень")
-    health = models.IntegerField("Здоровье")
-    strength = models.IntegerField("Сила")
-    defence = models.IntegerField("Защита")
-    stamina = models.IntegerField("Выносливость")
+    level = models.IntegerField("Уровень", blank=True, null=True)
+    health = models.IntegerField("Здоровье", blank=True, null=True)
+    strength = models.IntegerField("Сила", blank=True, null=True)
+    defence = models.IntegerField("Защита", blank=True, null=True)
+    stamina = models.IntegerField("Выносливость", blank=True, null=True)
 
     def __str__(self):
-        return f"{self.pokemon.title}: появился - {self.appeared_at.strftime('%Y-%m-%d-%H.%M.%S')}"
+        return f"{self.pokemon.title}({self.id})"
 
     class Meta:
         verbose_name = "Сущность покемона"
